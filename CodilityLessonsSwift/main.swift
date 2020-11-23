f89b9f2758dcf3d9364460ebbbcfeebf3b5c1556
Cătălin Sminticiuc <catalin.sminticiuc@gmail.com>
2020-11-23 18:18:01 +0200

    Lesson2 - OddOccurrencesInArray

--- CodilityLessonsSwift/main.swift
+++ CodilityLessonsSwift/main.swift
@@ -11,5 +11,8 @@ print("Hello, World!")
 
 //print("binary gap: ", solutionLesson1BinaryGap(1041))
 
-var A = [3, 8, 9, 7, 6]
-print(solutionLesson2CyclicRotation(&A, 3))
+//var A = [3, 8, 9, 7, 6]
+//print(solutionLesson2CyclicRotation(&A, 3))
+
+var A = [9, 3, 9, 3, 9, 7, 9]
+print(solutionLesson2OddOccurrencesInArray(&A))

1ff465557f891c028e432e3ab29ee493e06e6968
Cătălin Sminticiuc <catalin.sminticiuc@gmail.com>
2020-11-23 18:54:38 +0200

    Lesson3 - TapeEquilibrium

--- CodilityLessonsSwift/main.swift
+++ CodilityLessonsSwift/main.swift
@@ -11,13 +11,16 @@ print("Hello, World!")
 
 //print("binary gap: ", solutionLesson1BinaryGap(1041))
 
-//var A = [3, 8, 9, 7, 6]
+//var A = [3,8,9,7,6]
 //print(solutionLesson2CyclicRotation(&A, 3))
 
-//var A = [9, 3, 9, 3, 9, 7, 9]
+//var A = [9,3,9,3,9,7,9]
 //print(solutionLesson2OddOccurrencesInArray(&A))
 
-//print(solutionLesson3FrogJmp(10, 85, 30))
+//print(solutionLesson3FrogJmp(10,85,30))
 
-var A = [2,3,1,5]
-print(solutionLesson3PermMissingElem(&A))
+//var A = [2,3,1,5]
+//print(solutionLesson3PermMissingElem(&A))
+
+var A = [3,1,2,4,3]
+print(solutionLesson3TapeEquilibrium(&A))
